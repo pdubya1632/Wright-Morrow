55de639741a196783c2e1a63734d5f28b46536cc
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-09 15:28:03 -0700

    prettier reformat

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Home from './pages/Home';
 
 const client = new ApolloClient({
-    request: operation => {
+  request: (operation) => {
     const token = localStorage.getItem('id_token');
     operation.setContext({
-        headers:{
-        authorization: token ? `Bearer ${token}` :"",
-        }
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
     });
-    },
-    uri: "graphql",
-    });
+  },
+  uri: 'graphql',
+});
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-        <Router>
-            <>
-            {/* <Navbar /> */}
-            <Routes>        
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          {/* <Navbar /> */}
+          <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-            </Routes>
-            </>
-        </Router>
-        </ApolloProvider>
-    );
+            <Route
+              render={() => (
+                <h1 className="display-2">Wrong page!</h1>
+              )}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
-export default App; 
+export default App;
