fc374c9f668b75a3c8c4d4269c5296aba13acda0
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-09 09:58:57 -0700

    add Jobs template page

---
+++ client/src/pages/admin/Jobs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import JobsTable from "../../components/tables/JobsTable";
+
+export default function Jobs()  {
+  return (
+    <>
+        <JobsTable />
+    </>
+  );
+};
