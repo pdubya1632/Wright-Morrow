1062b6f574609922341f52692168a66f90a73644
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-08 22:20:29 -0700

    Admin : Side Nav Bar component Fixes #30

---
+++ client/src/components/nav/SidebarNav.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Sidebar, HiChartPie, HiViewBoards, HiInbox, HiUser, HiShoppingBag, HiArrowSmRight, HiTable } from 'flowbite-react';
+
+export default function SidebarNav() {
+    return (
+        <>
+            <div className="w-fit bg-gray-50">
+                <Sidebar aria-label="Sidebar with logo branding example">
+                    <Sidebar.Logo
+                    href="#"
+                    img="favicon.png"
+                    imgAlt="Flowbite logo"
+                    >
+                    Flowbite
+                    </Sidebar.Logo>
+                    <Sidebar.Items>
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiChartPie}
+                        >
+                        Dashboard
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiViewBoards}
+                        >
+                        Kanban
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiInbox}
+                        >
+                        Inbox
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiUser}
+                        >
+                        Users
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiShoppingBag}
+                        >
+                        Products
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiArrowSmRight}
+                        >
+                        Sign In
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                        href="#"
+                        icon={HiTable}
+                        >
+                        Sign Up
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                    </Sidebar.Items>
+                </Sidebar>
+            </div>
+        </>
+    );
+}
