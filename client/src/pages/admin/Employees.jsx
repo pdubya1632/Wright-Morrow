b32767f36f6f89b97463ecba56b42c01f23d4b31
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-09 09:57:27 -0700

    add Employees template page

---
+++ client/src/pages/admin/Employees.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import EmployeesTable from "../../components/tables/CustomersTable";
+
+export default function Employees()  {
+  return (
+    <>
+        <EmployeesTable />
+    </>
+  );
+};
