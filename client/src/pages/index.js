d1f0cae46729519c35874fe3d1b568a8c8ff842f
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-09 11:18:12 -0700

    Change logo name and temp comment out logo image

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 export const Home = React.lazy(() => import('./Home'));
 export const Admin = React.lazy(() => import('./admin/Admin'));
+export const Jobs = React.lazy(() => import('./admin/Jobs'));
 export const Customers = React.lazy(() =>
   import('./admin/Customers')
 );
 export const Employees = React.lazy(() =>
   import('./admin/Employees')
 );
-export const Jobs = React.lazy(() => import('./admin/Jobs'));
