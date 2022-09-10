be44c92a09de83f360e22939ebe03f90bd345f09
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-10 12:12:29 -0700

    update RequestForm again

--- client/src/components/forms/RequestForm.jsx
+++ client/src/components/forms/RequestForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Label, TextInput, Checkbox, Button } from 'flowbite-react';
+import { Card, Label, TextInput, Select, Textarea, Button } from 'flowbite-react';
 
 export function RequestForm() {
     return (
@@ -7,41 +7,130 @@ export function RequestForm() {
             <div className="max-w-sm">
                 <Card>
                     <form className="flex flex-col gap-4">
+                    {/* Email */}
                     <div>
                         <div className="mb-2 block">
                         <Label
-                            htmlFor="email1"
+                            htmlFor="email"
                             value="Your email"
                         />
                         </div>
                         <TextInput
-                        id="email1"
+                        id="email"
                         type="email"
-                        placeholder="name@flowbite.com"
+                        placeholder="name@email.com"
                         required={true}
                         />
                     </div>
+                    {/* Phone */}
                     <div>
                         <div className="mb-2 block">
                         <Label
-                            htmlFor="password1"
-                            value="Your password"
+                            htmlFor="phone"
+                            value="Your phone number"
                         />
                         </div>
                         <TextInput
-                        id="password1"
-                        type="password"
+                        id="phone"
+                        type="phone"
+                        placeholder="xxx-xxx-xxxx"
                         required={true}
                         />
                     </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="remember" />
-                        <Label htmlFor="remember">
-                        Remember me
-                        </Label>
+                    {/* Industry */}
+                    <div id="select">
+                        <div className="mb-2 block">
+                            <Label
+                            htmlFor="industry"
+                            value="Industry"
+                            />
+                        </div>
+                        <Select
+                            id="industry"
+                            required={true}
+                        >
+                            <option>Select...</option>
+                            <option>Personal</option>
+                            <option>Auction</option>
+                            <option>Retail Business</option>
+                            <option>Manufacturer</option>
+                            <option>Other</option>
+                        </Select>
                     </div>
+                    {/* Ship From */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="ship-from"
+                                value="Ship From"
+                            />
+                        </div>
+                        <TextInput
+                        id="ship-from"
+                        type="text"
+                        placeholder="Zip Code"
+                        required={true}
+                        />
+                    </div>
+                    {/* Ship To */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="ship-to"
+                                value="Ship To"
+                            />
+                        </div>
+                        <TextInput
+                        id="ship-to"
+                        type="text"
+                        placeholder="Zip Code"
+                        required={true}
+                        />
+                    </div>
+                    {/* Category */}
+                    <div id="select">
+                        <div className="mb-2 block">
+                            <Label
+                            htmlFor="category"
+                            value="Category of Items"
+                            />
+                        </div>
+                        <Select
+                            id="category"
+                            required={true}
+                        >
+                            <option>Select...</option>
+                            <option>Industrial</option>
+                            <option>Medical</option>
+                            <option>Technology</option>
+                            <option>Automotive</option>
+                            <option>Retail</option>
+                            <option>Art</option>
+                            <option>Antiques</option>
+                            <option>Furniture</option>
+                            <option>Residential Move</option>
+                            <option>Office Move</option>
+                            <option>Other</option>
+                        </Select>
+                    </div>
+                    {/* Description */}
+                    <div id="textarea">
+                        <div className="mb-2 block">
+                            <Label
+                            htmlFor="description"
+                            value="Description"
+                            />
+                        </div>
+                        <Textarea
+                            id="description"
+                            placeholder="Describe your items..."
+                            required={true}
+                            rows={4}
+                        />
+                    </div>
+                    {/* Submit */}
                     <Button type="submit">
-                        Submit
+                        Submit Request
                     </Button>
                     </form>
                 </Card>
