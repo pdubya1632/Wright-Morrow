0db0e43dd32c441cfd0649acc8cb2156d524c82b
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-10 11:25:08 -0700

    Home : Top Nav component Fixes #44

---
+++ client/src/components/layouts/TopNavLayout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navbar, Button } from 'flowbite-react';
+
+export function TopNavLayout() {
+    return (
+        <>
+            <Navbar fluid={true} rounded={true}>
+                <Navbar.Brand href="/">
+                {/* <img
+                    src="https://flowbite.com/docs/images/logo.svg"
+                    className="mr-3 h-6 sm:h-9"
+                    alt="Wright &amp; Morrow"
+                /> */}
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                    Wright &amp; Morrow
+                </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                <Button>Request A Quote</Button>
+                <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                <Navbar.Link href="/navbars" active={true}>
+                    Home
+                </Navbar.Link>
+                <Navbar.Link href="/navbars">Services</Navbar.Link>
+                <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+                <Navbar.Link href="/navbars">About</Navbar.Link>
+                <Navbar.Link href="/navbars">Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+};
