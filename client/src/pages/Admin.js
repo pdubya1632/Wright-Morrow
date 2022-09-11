34cd8d4db4eb128fec47db5bd7025f23715f3e79
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-10 21:36:31 -0700

    get jobs data in table

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-
-import { GET_JOBS } from '../utils/queries';
 
 import SidebarNav from '../components/nav/SidebarNav';
-
-function DisplayJobs() {
-  const { loading, error, data } = useQuery(GET_JOBS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.jobs.map(({ id, status, description }) => (
-    <div key={id}>
-      <h3>Status: {status}</h3>
-      <p>Description: {description}</p>
-      <br />
-    </div>
-  ));
-}
+import JobsTable from '../components/tables/JobsTable';
 
 export default function Admin() {
   return (
-    <div class="flex flex-row mt-6">
-      <div class="basis-1/4">
+    <div className="flex flex-row mt-6">
+      <div className="basis-1/4">
         <SidebarNav />
       </div>
-      <div class="basis-3/4">
-        <DisplayJobs />
+      <div className="basis-3/4">
+        <JobsTable />
       </div>
     </div>
   );
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,64 +7,60 @@ const typeDefs = gql`
     employeeId: String
     status: String
     pickupDate: String
-    pickupAddress:[Address]
-    dropOffAddress:[Address]
+    pickupAddress: [Address]
+    dropOffAddress: [Address]
     industry: String
     category: String
     description: String
     cost: String
     invoice: Int
-    ItemIds: [Item],
+    ItemIds: [Item]
   }
+
   type Address {
     _id: ID!
     street: String
     city: String
     state: String
     zip: String
-
   }
-
-  type Item{
+  type Item {
     _id: ID!
-  jobID: [Job]
-  name: String
-  value: String
-  length: String
-  width: String
-  height: String
-  weight: String
-  qty:  Int
-  packType: String
+    jobID: [Job]
+    name: String
+    value: String
+    length: String
+    width: String
+    height: String
+    weight: String
+    qty: Int
+    packType: String
   }
-
   type Customer {
     _id: ID!
     firstName: String
     lastName: String
-  phone: String
-  email: String
-  password: String
-  billingAddress: [Address]
-  jobIDs: [Job]
+    phone: String
+    email: String
+    password: String
+    billingAddress: [Address]
+    jobIDs: [Job]
   }
   type Employee {
-  _id: ID!
-  firstName: String
-  lastName: String
-  phone: String
-  email: String
-  password: String
-  isAdmin: Boolean
-  isActive: Boolean
-  jobIDs: [Job]
+    _id: ID!
+    firstName: String
+    lastName: String
+    phone: String
+    email: String
+    password: String
+    isAdmin: Boolean
+    isActive: Boolean
+    jobIDs: [Job]
   }
   type Auth {
     token: ID
     employee: Employee
   }
-  
-
 
   type Query {
     customers: [Customer]
@@ -83,29 +79,29 @@ const typeDefs = gql`
     addCustomer(
       firstName: String!
       lastName: String!
-      phone: String! 
+      phone: String!
       email: String!
-      password: String! 
-      jobIDs:  String
-      ): Customer
+      password: String!
+      jobIDs: String
+    ): Customer
     updateCustomer(
       _id: ID!
       firstName: String
       lastName: String
       phone: String
       email: String
-      password: String 
-      jobIDs:  String
-      ): Customer
+      password: String
+      jobIDs: String
+    ): Customer
     addEmployee(
       firstName: String!
-      lastName: String!  
+      lastName: String!
       phone: String!
       email: String!
       password: String!
-      jobIDs:  String
-      isAdmin:Boolean
-      ): Employee
+      jobIDs: String
+      isAdmin: Boolean
+    ): Employee
     updateEmployee(
       _id: ID!
       firstName: String
@@ -113,41 +109,39 @@ const typeDefs = gql`
       phone: String
       email: String
       password: String
-      jobIDs:  String
-      isAdmin:Boolean
-      ): Employee
-    addJob(  
+      jobIDs: String
+      isAdmin: Boolean
+    ): Employee
+    addJob(
       customerId: String
       employeeId: String
       status: String
       pickupDate: String
-      pickupAddress:String
-      dropOffAddress:String
+      pickupAddress: String
+      dropOffAddress: String
       industry: String
       description: String
       category: String
       cost: String
       invoice: Int
       ItemIds: String
-      ): Job
-      updateJob(
+    ): Job
+    updateJob(
       _id: ID!
       customerId: String
       employeeId: String
       status: String
       pickupDate: String
-      pickupAddress:String
-      dropOffAddress:String
+      pickupAddress: String
+      dropOffAddress: String
       industry: String
       category: String
       cost: String
       invoice: Int
       ItemIds: String
-      ): Job
-      deleteJob(
-      _id: ID!
-      ): Job
-    addItem(  
+    ): Job
+    deleteJob(_id: ID!): Job
+    addItem(
       _id: ID!
       jobID: String
       name: String
@@ -156,11 +150,11 @@ const typeDefs = gql`
       width: String
       height: String
       weight: String
-      qty:  Int
+      qty: Int
       packType: String
-      ) : Item
-      updateItem(
-        _id: ID!
+    ): Item
+    updateItem(
+      _id: ID!
       jobID: String
       name: String
       value: String
@@ -168,13 +162,11 @@ const typeDefs = gql`
       width: String
       height: String
       weight: String
-      qty:  Int
+      qty: Int
       packType: String
-      ) : Item
-      deleteItem(
-        _id: ID!
-      ) : Item
-      login(email: String!, password: String!): Auth
+    ): Item
+    deleteItem(_id: ID!): Item
+    login(email: String!, password: String!): Auth
   }
 `;
 
