14c536afc4df99cff34eef5d0af9f6e906884aef
Angela W <squittenplease@gmail.com>
2022-09-13 21:50:49 -0700

    Fix Job Model

--- server/schemas/resolvers/job.js
+++ server/schemas/resolvers/job.js
@@ -1,4 +1,4 @@
-const { Jobs } = require('../../models');
+const { Job } = require('../../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
