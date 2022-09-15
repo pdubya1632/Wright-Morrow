a7b941c0ba0e76e862bba3ddce91c2087fe9efe4
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-14 23:18:16 -0700

    Home : finalize styling of Login & Register forms Fixes #67

---
+++ client/src/components/layout/AuthFormHeader.jsx
@@ -0,0 +1,10 @@
+export function AuthFormHeader() {
+  return (
+
+<header class="flex items-center justify-between mt-20">
+        <div class="h-6 text-2xl font-semibold block w-full text-center">
+          <a href="/">Wright &amp; Morrow</a>
+        </div>
+      </header>
+  )
+};
