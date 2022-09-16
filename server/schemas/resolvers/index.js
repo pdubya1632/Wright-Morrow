26ca259f04863e061c86ac22205e2a22a74a01e7
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-15 20:19:04 -0700

    Admin : fix items & employees tables not pulling in data Fixes #70

--- server/schemas/resolvers/index.js
+++ server/schemas/resolvers/index.js
@@ -2,18 +2,20 @@ const employeeResolvers = require('./employee.js');
 const jobResolvers = require('./job.js');
 const customerResolvers = require('./customer.js');
 const requestResolvers = require('./request.js');
+const itemResolvers = require('./item.js');
 
 module.exports = {
   Query: {
     ...employeeResolvers.Query,
     ...jobResolvers.Query,
     ...customerResolvers.Query,
+    ...itemResolvers.Query,
     ...requestResolvers.Query,
   },
   Mutation: {
     ...employeeResolvers.Mutation,
     ...jobResolvers.Mutation,
     ...customerResolvers.Mutation,
-    ...requestResolvers.Mutation
-  }
-}
+    ...requestResolvers.Mutation,
+  },
+};
