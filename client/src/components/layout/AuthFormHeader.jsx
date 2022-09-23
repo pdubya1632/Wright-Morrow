b6d792c1a4824073f728bcc7908beb058624691a
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-22 20:47:18 -0700

    logo link to home page

--- client/src/components/layout/AuthFormHeader.jsx
+++ client/src/components/layout/AuthFormHeader.jsx
@@ -1,10 +1,10 @@
 import { ReactComponent as Logo } from '../../images/wm-logo.svg';
+import { Link } from 'react-router-dom';
 
 export function AuthFormHeader() {
   return (
     <header className="mt-20 mb-5 flex justify-center w-full">
-        <Logo width={175} alt="Wright &amp; Morrow" className="" />
-        <a href="/" className="text-2xl font-semibold hidden">Wright &amp; Morrow</a>
+        <Link to="/"><Logo width={175} alt="Wright &amp; Morrow" className="" /></Link>
     </header>
   );
 }
