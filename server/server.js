dbf964a0e1bafc7e5ec8ae1380b7f2c567d3c5d6
Angela W <squittenplease@gmail.com>
2022-09-25 19:46:43 -0700

    Fix code for heroku

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
