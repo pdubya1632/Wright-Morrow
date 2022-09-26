90ea1a11bbcf7d5cd67c3ffdccf32063271a7b83
Angela W <squittenplease@gmail.com>
2022-09-25 19:17:01 -0700

    Fix database, add MongoDB cloud
    This is the cloud version of the database. You must use my password and input string to access. I will see if I can add you as users to this schema.
    
    Also, this is in the mgmt_db, not the wrightandmorrow one. I noticed after I transferred everything. Will create pull request to fix at later data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// This is using my .env file to connect to my MongoDB database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wrightandmorrow', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
