d0ab64037463e5e4b151cb19a2702bd259e2ab66
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-25 20:44:06 -0700

    remove duplicate NavLink declaration

--- client/src/components/nav/TopNav.jsx
+++ client/src/components/nav/TopNav.jsx
@@ -2,7 +2,6 @@ import { Navbar, Button } from 'flowbite-react';
 import { NavLink } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../images/wm-logo.svg';
-import { NavLink } from 'react-router-dom';
 
 export function TopNav() {
   return (
